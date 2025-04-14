fd3ba14551108aad36a3a03f8642d6c01640f316
Mark Otto <motto@Rufa.local>
2025-04-14 17:58:48 -0600

    Changed the cYFS so it sorts frame first as the output tables are.  It resolved "variable not consistent with parent" errors.

--- data/MetaVars.R
+++ data/MetaVars.R
@@ -1,84 +1,26 @@
 CurrentYr <-
-  2023
+2023
 cYear <-
-  c(2015, 2023)
+c(2015, 2023)
 nYear <-
-  2L
+2L
 cFrame <-
-  c("List", "Area")
+c("List", "Area")
 nFrame <-
-  2L
+2L
 cStr <-
-  c("SC", "Co", "ND")
+c("SC", "Co", "ND")
 nStr <-
-  3L
+3L
 cYrStr <-
-  c("2015.SC",
-    "2015.Co",
-    "2015.ND",
-    "2023.SC",
-    "2023.Co",
-    "2023.ND")
+c("2015.SC", "2015.Co", "2015.ND", "2023.SC", "2023.Co", "2023.ND"
+)
 nYrStr <-
-  6L
+6L
 cYFS <-
-  c(
-    "2015.List.SC",
-    "2015.List.Co",
-    "2015.List.ND",
-    "2015.Area.SC",
-    "2015.Area.Co",
-    "2015.Area.ND",
-    "2023.List.SC",
-    "2023.List.Co",
-    "2023.List.ND",
-    "2023.Area.SC",
-    "2023.Area.Co",
-    "2023.Area.ND"
-  )
+c("2015.List.SC", "2015.List.Co", "2015.List.ND", "2023.List.SC", 
+"2023.List.Co", "2023.List.ND", "2015.Area.SC", "2015.Area.Co", 
+"2015.Area.ND", "2023.Area.SC", "2023.Area.Co", "2023.Area.ND"
+)
 nYFS <-
-  12L
-StrArea <-
-  structure(
-    list(
-      Str_PK = c("SC", "Co", "ND"),
-      cStratum = c("South Central", "Coastal", "North and Downeast"),
-      KMSq = c(17602.905886034401, 6745.9621559483603, 59489.682796224297)
-    ),
-    class = "data.frame",
-    row.names = c("SC", "Co", "ND")
-  )
-fStr <-
-  structure(1:3, levels = c("SC", "Co", "ND"), class = "factor")
-cSeat <-
-  c("Front", "Rear", "Dirt")
-cObsr <-
-  c(
-    "SB",
-    "BC",
-    "MC",
-    "AD",
-    "DD",
-    "AF",
-    "VH",
-    "CK",
-    "DK",
-    "AM",
-    "KM",
-    "LM",
-    "BP",
-    "RP",
-    "JR",
-    "AS",
-    "CS",
-    "KS",
-    "SS",
-    "CT",
-    "MT",
-    "BCU",
-    "CW",
-    "BZ",
-    "Gnd"
-  )
-nObsr <-
-  25L
+12L
